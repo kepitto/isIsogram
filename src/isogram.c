41235bd81dff868e8862b1d3eb07c84ec5e1d97a
Mathias Rieder <mathias.rieder@gmail.com>
2020-05-06 10:59:30 +0000

    fixed spaces and casing

--- src/isogram.c
+++ src/isogram.c
@@ -1,37 +1,33 @@
 #include "isogram.h"
 #include "string.h"
 
-// count array[0-255] anzahl des jeweiligen Zeichens im String
-    /* forEach( character in phrase){
-        count[character]++
-    }
-    if (aynthing in count > 1) {
-        return false
-    }
-    return true
-*/
+bool isIgnored(char c) {
+    return c == ' ';
+}
 
-/*
-    [ W o r d ]
-      ^
-      ^ ^ ^ ^ // w?
-        ^ ^ ^ // o?
-*/
+char toLowerCase(char c){
+    if (c >= 'A' && c <= 'Z') {
+        return c - ('A' - 'a');
+    }
+    return c;
+}
 
 bool is_isogram(const char phrase[]){
     int len = strlen(phrase);
     for (int i=0; i<len; i++) {
-        char currentChar = phrase[i];
-        //check all other chars against currentChar
-        //find if there are more than 1 occurences
-        int occurences = 0;
-        for(int j=0; j<len; j++){
-            if (phrase[j] == currentChar) {
-                occurences++;
+        char currentChar = toLowerCase(phrase[i]);
+        if (!isIgnored(currentChar)){
+            //check all other chars against currentChar
+            //find if there are more than 1 occurences
+            int occurences = 0;
+            for(int j=0; j<len; j++){
+                if (toLowerCase(phrase[j]) == currentChar) {
+                    occurences++;
+                }
             }
-        }
-        if (occurences > 1) { //found duplicate characters --> no isogram
-            return false;
+            if (occurences > 1) { //found duplicate characters --> no isogram
+                return false;
+            }
         }
     }
     // we didnt find duplicates, must be an isogram
